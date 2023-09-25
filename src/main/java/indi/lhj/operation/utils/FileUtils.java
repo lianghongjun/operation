1bd40db68657f64504a108c1b5e0293779fe0f73
lianghongjun <1317206391@qq.com>
2023-09-25 20:14:14 +0800

    FileUtils文件工具类创建

---
+++ src/main/java/indi/lhj/operation/utils/FileUtils.java
@@ -0,0 +1,82 @@
+package indi.lhj.operation.utils;
+
+import indi.lhj.operation.exception.OperandException;
+import indi.lhj.operation.pojo.Equation;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 文件工具类
+ *
+ * @author 郑耿杭，梁鸿俊
+ */
+public class FileUtils {
+
+    /**
+     * 将内容写入指定路径文件
+     *
+     * @param path    文件路径
+     * @param context 写入内容
+     * @return 成功与否
+     */
+    public static boolean write(String path, List<String> context) {
+        try {
+            List<String> list = new ArrayList<>();
+            // 加上序号
+            for (int i = 0; i < context.size(); i++) {
+                list.add(i, i + "、" + context.get(i));
+            }
+            Files.write(Paths.get(path), list);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 从指定路径文件读取成表达式数组
+     *
+     * @param path 文件路径
+     * @return 表达式数组
+     */
+    public static List<Equation> readAsEquation(String path) {
+        List<Equation> lines;
+        try {
+            lines = Files.readAllLines(Paths.get(path)).stream().map(s -> {
+                try {
+                    // 除去序号
+                    return Equation.parse(s.substring(s.indexOf("、") + 1));
+
+                } catch (OperandException e) {
+                    throw new RuntimeException(e);
+                }
+            }).toList();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return lines;
+    }
+
+    public static List<Float> readAsAnswer(String path) {
+        List<Float> lines;
+        try {
+            // 除去序号
+            lines = Files.readAllLines(Paths.get(path)).stream()
+                    .map(s -> Float.parseFloat(s.substring(s.indexOf("、") + 1))).toList();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return lines;
+    }
+}
